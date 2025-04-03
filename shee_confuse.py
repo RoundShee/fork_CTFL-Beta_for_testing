08394163c987498ff06707726f0f6369e2040ff8
RoundShee <roundsheep@qq.com>
2025-04-03 23:14:04 +0800

    结果暂时展示 确实存在问题 该修改数据了 从头开始修改

--- shee_confuse.py
+++ shee_confuse.py
@@ -4,6 +4,9 @@ from torch.utils.data.dataloader import DataLoader
 from dataset import DownDataset
 from model import Model, DownStreamModel
 import torch.nn.functional as F
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def get_confuse_matrix(pre_model_path='./checkpoints/epoch80_checkpoint_pretrain_model_bs128.pth',
@@ -22,13 +25,25 @@ def get_confuse_matrix(pre_model_path='./checkpoints/epoch80_checkpoint_pretrain
     # 加载测试数据
     dataset = DownDataset(test_path, split_num=100)
     dataloader = DataLoader(dataset, batch_size=16)
-    # test
+    all_real_label = []
+    all_pre_label = []
     for data, label in dataloader:
-        data, label = data.to(device), label.to(device)
+        all_real_label.extend(label.tolist())
+        data = data.to(device)
         out = model(data)
         pre_label = F.softmax(out, dim=-1).max(-1)[1]
-        print(pre_label, label)  # tensor([0], device='cuda:0') tensor([0], device='cuda:0')
+        # print(pre_label, label)  # tensor([0], device='cuda:0') tensor([0], device='cuda:0')
+        all_pre_label.extend(pre_label.tolist())
+    matrix = confusion_matrix(all_real_label, all_pre_label)
+    return matrix
 
 
 if __name__ == '__main__':
-    get_confuse_matrix()
+    conf_matrix = get_confuse_matrix(test_path='./data/TFIs30_10/final/n6')
+    # 可视化混淆矩阵
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues')
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+    plt.show()

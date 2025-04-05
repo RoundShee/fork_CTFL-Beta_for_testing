1fb0189b4c657a9a3d26873f921b28a872d87cf8
RoundShee <roundsheep@qq.com>
2025-04-05 16:39:40 +0800

    尝试老模型新数据测试, 混淆矩阵增加俩评价标准, 计划再次从理论上认知算法, 怀疑当前MSST算法部分

--- shee_confuse.py
+++ shee_confuse.py
@@ -4,13 +4,13 @@ from torch.utils.data.dataloader import DataLoader
 from dataset import DownDataset
 from model import Model, DownStreamModel
 import torch.nn.functional as F
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, normalized_mutual_info_score, adjusted_rand_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
 def get_confuse_matrix(pre_model_path='./checkpoints/epoch80_checkpoint_pretrain_model_bs128.pth',
-                       model_path='./checkpoints/epoch160_down_model.pth',
+                       model_path='./checkpoints/epoch100_down_model.pth',
                        test_path='./data/TFIs30_10/final/p2'):
     # 加载模型
     args = load_args()
@@ -35,15 +35,22 @@ def get_confuse_matrix(pre_model_path='./checkpoints/epoch80_checkpoint_pretrain
         # print(pre_label, label)  # tensor([0], device='cuda:0') tensor([0], device='cuda:0')
         all_pre_label.extend(pre_label.tolist())
     matrix = confusion_matrix(all_real_label, all_pre_label)
-    return matrix
+    accuracy = accuracy_score(all_real_label, all_pre_label)
+    nmi = normalized_mutual_info_score(all_real_label, all_pre_label)
+    ari = adjusted_rand_score(all_real_label, all_pre_label)
+    return matrix, accuracy, nmi, ari
 
 
 if __name__ == '__main__':
-    conf_matrix = get_confuse_matrix(test_path='./data/TFIs30_10/final/n6')
+    conf_matrix, accuracy, nmi, ari = get_confuse_matrix(
+        pre_model_path='./checkpoints/epoch80_pretrain20250404.pth',
+        model_path='./checkpoints/epoch100_down20250404.pth',
+        test_path='./data/TFIs30_10/final/p2')
     # 可视化混淆矩阵
     plt.figure(figsize=(8, 6))
     sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues')
     plt.xlabel('Predicted Labels')
     plt.ylabel('True Labels')
     plt.title('Confusion Matrix')
+    plt.suptitle(f'SNR=2dB Accuracy={accuracy:.2f}, NMI={nmi:.2f}, ARI={ari:.2f}')
     plt.show()
